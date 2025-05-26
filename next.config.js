11ab9611f60c9fbcbda6aa62a8a9143b9a7b2ed4
banlogto <banlogno@gmail.com>
2025-05-26 13:23:11 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
 
+module.exports = {
+  output: 'export',
+  assetPrefix: isProd ? '/your-repo-name/' : '',
+  images: {
+    unoptimized: true,
+  },
+}
